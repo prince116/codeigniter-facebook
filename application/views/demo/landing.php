838bad2c1ff3dfe03fc19c0e72a3085f6a56c732
prince116 <princewong116@gmail.com>
2017-09-13 14:43:59 +0800

    update

--- application/views/demo/landing.php
+++ application/views/demo/landing.php
@@ -1,4 +1,19 @@
-<h1 class="text-center">Hello World</h1>
+
+<div class="container">
+    
+    <div class="row">
+        <div class="well">
+            <h1 class="text-center">Hello World</h1>
+        </div>
+    </div>
+    
+    
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+            <button type="button" class="btn btn-default btn-primary" id="btn-login">Facebook Login</button>
+        </div>
+    </div>
+    
+</div>
 
 
-<button type="button" class="btn btn-default" id="btn-login">Facebook Login</button>
