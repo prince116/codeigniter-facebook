13b857523327736e2c10884537a179413b38e7b4
prince116 <princewong116@gmail.com>
2017-09-14 10:26:10 +0800

    fix

--- application/views/demo/landing.php
+++ application/views/demo/landing.php
@@ -12,26 +12,6 @@
             <button type="button" class="btn btn-default btn-primary" id="btn-login">Facebook Login</button>
         </div>
     </div>
-
-    <div class="row">
-        
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            
-            <?php echo form_open('news/create'); ?>
-            
-                <div class="form-group">
-                    <label for="title">Title</label>
-                    <input type="text" name="title" class="form-control" />
-                </div>
-            
-                <input type="submit" name="submit" value="Create news item" />
-            
-            </form>
-            
-
-        </div>
-        
-    </div>
     
 </div>
 
